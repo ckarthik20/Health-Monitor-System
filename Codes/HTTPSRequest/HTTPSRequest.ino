2eb142a8a9c88f7f255deda18f7d785625769f9e
ckarthik20 <ckarthik@live.com>
2016-08-25 12:07:20 +0530

    Add files via upload
    IoT based Health monitor System

---
+++ Codes/HTTPSRequest/HTTPSRequest.ino
@@ -0,0 +1,202 @@
+
+#include <ESP8266WiFi.h>
+const char* ssid     = "Ranirajan";
+const char* password = "sundara2006";
+const char* host = "192.168.1.102";
+const int httpPort = 80;
+String PostData="";
+String PostData1="";
+boolean capture=false;
+int ledState = LOW;  
+int counter = 0;
+String ECG="";
+String PULSE1="";
+String TEMP="";
+String AIRFLO="";
+WiFiClient client;
+uint32_t id=ESP.getChipId();
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+      digitalWrite(LED_BUILTIN, LOW);   
+      delay(500);                      
+      digitalWrite(LED_BUILTIN, HIGH); 
+      delay(500);   
+  }              
+  digitalWrite(LED_BUILTIN, HIGH);  
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+  // Use WiFiClient class to create TCP connections
+  if (!client.connect(host, httpPort)) {
+    digitalWrite(LED_BUILTIN, LOW);   
+      delay(1000);                      
+      digitalWrite(LED_BUILTIN, HIGH); 
+      delay(1000);  
+    return;
+   }
+  if(counter==0)
+   {
+    Serial.print("ECG");
+    do
+    {
+    for(int i=0; i<1; i++)
+      {      
+      ECG =Serial.readString();
+      Serial.print(ECG);
+      delay(5);
+      }
+     counter=1;
+   }while(Serial.available()>0);
+   }
+   else if(counter==1)
+    {
+        Serial.print("PULSE");
+    do
+    {
+    for(int j=0; j<1; j++)
+      {      
+      PULSE1 =Serial.readString();
+      Serial.print(PULSE1);
+      delay(2000);
+      }
+     counter=2;
+   }while(Serial.available()>0);
+   }
+   else if(counter==2)
+    {
+        Serial.print("TEMP");
+    do
+    {
+    for(int k=0; k<1; k++)
+      {      
+      TEMP =Serial.readString();
+      Serial.print(TEMP);
+      delay(5);
+      }
+     counter=3;
+   }while(Serial.available()>0);
+    }
+   else if(counter==3)
+    {
+        Serial.print("AIRFLO");
+    do
+    {
+    for(int l=0; l<1; l++)
+      {      
+      AIRFLO =Serial.readString();
+      Serial.print(AIRFLO);
+      delay(5);
+      }
+     counter=4;
+   }while(Serial.available()>0);
+    }
+   else if(counter==4)
+    {
+        Serial.print("Success");
+        delay(500);
+           PostData = "ECG=" + ECG + "&PULSE=" + PULSE1 + "&TEMP=" + TEMP  + "&DEVICE=" + id;
+  PostData1 = "AIRFLO=" + AIRFLO + "&DEVICE=" + id;
+        delay(800);
+     post();
+     post1();
+     counter=5;
+    }
+   
+} 
+void post()
+{
+  if (client.connect(host,80)) {
+client.println("POST /secure/esp.php HTTP/1.1");
+client.println("Host: 192.168.1.102");
+client.println("Cache-Control: no-cache");
+client.println("Content-Type: application/x-www-form-urlencoded");
+client.println("Connection: close");
+client.print("Content-Length: ");
+client.println(PostData.length());
+client.println();
+client.println(PostData);
+client.println();
+
+long interval = 2000;
+unsigned long currentMillis = millis(), previousMillis = millis();
+
+while(!client.available()){
+
+  if( (currentMillis - previousMillis) > interval ){
+         digitalWrite(LED_BUILTIN, LOW);   
+      delay(1000);                      
+      digitalWrite(LED_BUILTIN, HIGH); 
+      delay(1000);  
+   // Serial.println("Timeout");
+    client.stop();     
+    return;
+  }
+  currentMillis = millis();
+}
+
+while (client.connected())
+ {
+  if ( client.available() )
+  {
+    char str=client.read();
+   //Serial.println(str);
+  }      
+ }
+   }
+    }
+void post1()
+{
+  if (client.connect(host,80)) {
+client.println("POST /secure/airflow.php HTTP/1.1");
+client.println("Host: 192.168.1.102");
+client.println("Cache-Control: no-cache");
+client.println("Content-Type: application/x-www-form-urlencoded");
+client.println("Connection: close");
+client.print("Content-Length: ");
+client.println(PostData1.length());
+client.println();
+client.println(PostData1);
+client.println();
+
+long interval = 2000;
+unsigned long currentMillis = millis(), previousMillis = millis();
+
+while(!client.available()){
+
+  if( (currentMillis - previousMillis) > interval ){
+         digitalWrite(LED_BUILTIN, LOW);   
+      delay(1000);                      
+      digitalWrite(LED_BUILTIN, HIGH); 
+      delay(1000);  
+   // Serial.println("Timeout");
+    client.stop();     
+    return;
+  }
+  currentMillis = millis();
+}
+
+while (client.connected())
+ {
+  if ( client.available() )
+  {
+    char str=client.read();
+   //Serial.println(str);
+  }      
+ }
+   }
+    }
+
+
+
---
+++ Codes/Health_Care_System_v1/Health_Care_System_v1.ino
@@ -0,0 +1,158 @@
+#include <math.h>
+#define pulsePin A1
+
+int av,avg;
+float pad =9900;
+float thermr = 10000;
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// Regards Serial OutPut  -- Set This Up to your needs
+static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
+
+int ld;
+
+void setup()
+{
+ //for led assign on every process
+ for(ld=4;ld<8;ld++)
+  { pinMode(ld,OUTPUT);
+  }
+  
+  Serial.begin(115200);
+  pinMode(10, INPUT);  pinMode(11, INPUT); //for ECG 10-> LO+; 11->LO-
+
+delay(1000);
+    
+interruptSetup();
+
+ 
+  
+}
+
+void loop() {
+if(Serial.available()>0)
+{
+  String command=Serial.readString();
+  if(command=="ECG")
+   {
+     ecg();
+     ld=4;
+     light(ld);
+     delay(2000);
+   }
+  else if(command=="PULSE")
+   {
+    int i=0;
+    do 
+     {
+       pulse();
+       av=av+BPM;
+       i++;
+     }while(i<5);
+    avg=av/5; 
+    Serial.println(avg);
+    ld=5;
+    light(ld);
+    delay(2000);
+   }
+  else if(command=="TEMP")
+   {
+     temperature();
+     ld=6;
+     light(ld);
+     delay(2000);
+   }
+  else if(command=="AIRFLO")
+   {
+     airflo();
+     ld=7;
+     light(ld);
+     delay(2000);
+   }
+   else if(command=="Success")
+   {
+    success();
+   }
+ }
+}
+void ecg()
+  {
+    
+   
+        if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+       // break;
+        Serial.println('!');
+          }
+          
+        else{
+     for(int i=0; i<500; i++)
+      {      
+      Serial.println(analogRead(A0));
+      Serial.print(",");
+       delay(5);
+      }
+       
+      }
+      }
+
+
+  void airflo()
+  {
+  for(int i=0; i<300; i++)
+    {
+      Serial.println(analogRead (A3));
+      delay(100);
+    }
+  }
+
+
+void temperature() {
+float temp=analogRead(A2);
+long Resistance;  
+float Temp;
+Resistance=pad*((1024.0 / temp) - 1);
+Temp = log(Resistance);
+Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
+Temp = Temp - 273.15;  // Convert Kelvin to Celsius                      
+Serial.println(Temp);
+}
+
+
+void pulse()
+{
+     serialOutput() ;       
+    
+  if (QS == true){     // A Heartbeat Was Found
+
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
+        QS = false;                      // reset the Quantified Self flag for next time    
+  }
+     
+  delay(20);                             //  take a break
+}
+
+void light(int led)
+{
+digitalWrite(led,HIGH);
+delay(2000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+}
+
+void success(){
+
+   for(ld=4;ld<8;ld++)
+  { digitalWrite(ld,HIGH);
+    delay(800);
+   digitalWrite(ld,LOW);
+  
+  }
+}
+
